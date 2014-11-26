101054e37076f91a79d249e3e1e6d52a39089261
Ivan Pavkovic <pavkovic.ri@gmail.com>
2014-11-26 23:59:40 +0100

    S3 API implementation & test fully functional

--- middleware/src/test/java/at/tuwien/aic/raid/S3SampleTest.java
+++ middleware/src/test/java/at/tuwien/aic/raid/S3SampleTest.java
@@ -2,24 +2,113 @@ package at.tuwien.aic.raid;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
 
 import at.tuwien.aic.raid.connector.S3Connector;
+import at.tuwien.aic.raid.data.FileObject;
 
 public class S3SampleTest {
 
-	@Test
-	public void test() {
-		String[] a=new String[0];
+    @Test
+    public void createFileTest() {
+    	
+    	FileObject f;
+    	f = new FileObject() ;
+    	byte[] b = "AAAA".getBytes();
+
+
+        S3Connector s = new S3Connector();
+		f.setData(b);
+		f.setName("Name6");
+		
+		s.create(f);
 		try {
-			S3Connector.main(a);
+			FileObject fr = s.read(f);			
+			assertArrayEquals(b, fr.getData());
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			fail(e.getMessage());
 			e.printStackTrace();
-		}
+		}		
 
-	}
+    }
+    
+    @Test()
+   public void readFileTest() throws IOException {
+    	
+    	FileObject f;
+    	f = new FileObject() ;
+    	byte[] b = "Any String you want7".getBytes();
 
+
+        S3Connector s = new S3Connector();
+		f.setData(b);
+		f.setName("Name6");
+		
+		s.read(f);
+
+    }
+    @Test
+    public void deleteFileTest() throws IOException {
+    	
+    	FileObject f;
+    	f = new FileObject() ;
+		f.setName("Name6");
+		
+		S3Connector s = new S3Connector();
+		s.delete(f);
+		try {
+			s.delete(f);
+			
+		} catch (IOException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}		
+
+    }
+    
+    @Test
+    public void updateFileTest() {
+    	
+    	FileObject f;
+    	f = new FileObject() ;
+    	byte[] b = "Test".getBytes();
+
+
+        S3Connector s = new S3Connector();
+		f.setData(b);
+		f.setName("Name6");
+		
+		s.create(f);
+		try {
+			s.update(f);
+
+			
+		} catch (IOException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}		
+
+    }
+    
+    
+
+//    @Test
+//    public void bLaTest(){
+//        String[] a=new String[0];
+//        try {
+//
+//            S3Connector s = new S3Connector();
+//            s.listFiles();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
 }
